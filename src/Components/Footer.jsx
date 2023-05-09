88503f01ffedcaaa71ce62ab5c49a643cf7fad3a
THammon <tristen.hammon21@gmail.com>
2023-05-08 21:28:58 -0500

    footer fixed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,80 +2,98 @@ import React from 'react';
 import {
   MDBFooter,
   MDBContainer,
-  MDBIcon,
   MDBBtn
 } from 'mdb-react-ui-kit';
+import twiticon from "../twittericon.png"
+import insticon from "../instaicon.png"
+import googicon from "../googleicon.png"
+import linkicon from "../linkedinicon.png"
+import giticon from "../githubicon.png"
+import faceicon from "../facebookicon.png"
 
-export default function Footer() {
+export default function App() {
   return (
-    <MDBFooter className='bg-light text-center text-white'>
-      <MDBContainer className='p-4 pb-0'>
+    <MDBFooter className='text-center text-white' style={{ backgroundColor: '#f1f1f1' }}>
+      <MDBContainer className='pt-4'>
         <section className='mb-4'>
           <MDBBtn
+            rippleColor="dark"
+            color='link'
             floating
-            className='m-1'
-            style={{ backgroundColor: '#3b5998' }}
-            href='#!'
+            size="lg"
+            className='text-dark m-1'
+            href='https://www.facebook.com'
             role='button'
           >
-            <MDBIcon fab icon='facebook-f' />
+            <img src={faceicon} alt="facebookicon" width= '35px'/>
           </MDBBtn>
 
           <MDBBtn
+            rippleColor="dark"
+            color='link'
             floating
-            className='m-1'
-            style={{ backgroundColor: '#55acee' }}
-            href='#!'
+            size="lg"
+            className='text-dark m-1'
+            href='https://www.twitter.com'
             role='button'
           >
-            <MDBIcon fab icon='twitter' />
+            <img src={twiticon} alt="twittericon" width= '30px'/>
           </MDBBtn>
 
           <MDBBtn
+            rippleColor="dark"
+            color='link'
             floating
-            className='m-1'
-            style={{ backgroundColor: '#dd4b39' }}
-            href='#!'
+            size="lg"
+            className='text-dark m-1'
+            href='https://www.google.com'
             role='button'
           >
-            <MDBIcon fab icon='google' />
-          </MDBBtn>
-          <MDBBtn
-            floating
-            className='m-1'
-            style={{ backgroundColor: '#ac2bac' }}
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab icon='instagram' />
+            <img src={googicon} alt="googleicon" width= '30px'/>
           </MDBBtn>
 
           <MDBBtn
+            rippleColor="dark"
+            color='link'
             floating
-            className='m-1'
-            style={{ backgroundColor: '#0082ca' }}
-            href='#!'
+            size="lg"
+            className='text-dark m-1'
+            href='https://www.instagram.com'
             role='button'
           >
-            <MDBIcon fab icon='linkedin-in' />
+            <img src={insticon} alt="instagramicon" width= '30px'/>
           </MDBBtn>
 
           <MDBBtn
+            rippleColor="dark"
+            color='link'
             floating
-            className='m-1'
-            style={{ backgroundColor: '#333333' }}
-            href='#!'
+            size="lg"
+            className='text-dark m-1'
+            href='https://www.linkedin.com'
             role='button'
           >
-            <MDBIcon fab icon='github' />
+            <img src={linkicon} alt="linkedinicon" width= '30px'/>
+          </MDBBtn>
+
+          <MDBBtn
+            rippleColor="dark"
+            color='link'
+            floating
+            size="lg"
+            className='text-dark m-1'
+            href='https://www.github.com'
+            role='button'
+          >
+            <img src={giticon} alt="githubicon" width= '35px'/>
           </MDBBtn>
         </section>
       </MDBContainer>
 
-      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+      <div className='text-center text-dark p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
         © 2023 Copyright:
-        <a className='text-white' href='https://hobbylobby.com/'>
-         Pinkscissortails.org
+        <a className='text-dark' href='https://www.hobbylobby.com/'>
+          PinkScissortails.com
         </a>
       </div>
     </MDBFooter>
